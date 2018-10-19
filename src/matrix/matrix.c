8ae349b7ec003e647f77fb58951d47990e480a3e
Hugo <hugo.rosenkranz@gmail.com>
2018-10-19 14:34:13 +0200

    Update matrix.c

--- src/matrix/matrix.c
+++ src/matrix/matrix.c
@@ -9,8 +9,7 @@ Matrix initMatrix(size_t l, size_t m, bool random){
   M.shape[1] = m;
   M.data = (Vector*) calloc (l, sizeof(Vector));
 
-  size_t i;
-  for(i = 0; i < l; i++){
+  for(size_t i = 0; i < l; i++){
     M.data[i] = initVector(m, random);
   }
 
@@ -18,15 +17,13 @@ Matrix initMatrix(size_t l, size_t m, bool random){
 }
 
 void printMatrix(Matrix m){
-  int i;
-  for(i = 0; i < m.shape[0]; i++){
+  for(size_t i = 0; i < m.shape[0]; i++){
     printVector(m.data[i]);
   }
 }
 
 void freeMatrix(Matrix m){
-  int i;
-  for(i = 0; i < m.shape[0]; i++){
+  for(size_t i = 0; i < m.shape[0]; i++){
     freeVector(m.data[i]);
   }
 }
@@ -36,8 +33,8 @@ Matrix addMatrix(Matrix a, Matrix b){
     return initMatrix(0, 0, false);
 
   Matrix res = initMatrix(a.shape[0], a.shape[1], false);
-  int i;
-  for(i = 0; i < a.shape[0]; i++){
+
+  for(size_t i = 0; i < a.shape[0]; i++){
     res.data[i] = addVector(a.data[i], b.data[i]);
   }
 
@@ -47,8 +44,8 @@ Matrix addMatrix(Matrix a, Matrix b){
 
 Matrix scalarMatrix(Matrix m, double s){
   Matrix res = initMatrix(m.shape[0], m.shape[1], false);
-  int i;
-  for(i = 0; i < m.shape[0]; i++){
+
+  for(size_t i = 0; i < m.shape[0]; i++){
     res.data[i] = scalarVector(m.data[i], s);
   }
 
@@ -60,8 +57,8 @@ Matrix multMatrix(Matrix a, Matrix b){
     return initMatrix(0, 0, false);
 
   Matrix res = initMatrix(a.shape[0], a.shape[1], false);
-  int i;
-  for(i = 0; i < a.shape[0]; i++){
+
+  for(size_t i = 0; i < a.shape[0]; i++){
     res.data[i] = multVector(a.data[i], b.data[i]);
   }
 
@@ -72,10 +69,10 @@ Matrix dotMatrix(Matrix a, Matrix b){
   if (a.shape[1] != b.shape[0])
     return initMatrix(0, 0, false);
 
-  unsigned int l = a.shape[0];
-  unsigned int m = b.shape[1]; 
+  size_t l = a.shape[0];
+  size_t m = b.shape[1]; 
   Matrix res = initMatrix(l, m, false);
-  int i, j, k;
+  size_t i, j, k;
 
   Vector v;
   for(i = 0; i < l; i++){
@@ -91,11 +88,11 @@ Matrix dotMatrix(Matrix a, Matrix b){
 }
 
 Matrix transpose(Matrix m){
-  unsigned int l = m.shape[0];
-  unsigned int k = m.shape[1]; 
+  size_t l = m.shape[0];
+  size_t k = m.shape[1]; 
   Matrix res = initMatrix(k, l, false);
 
-  int i, j;
+  size_t i, j;
 
   for(i = 0; i < l; i++){
     for(j = 0; j < k; j++){
@@ -109,8 +106,8 @@ Matrix transpose(Matrix m){
 
 Matrix sigmoidMatrix(Matrix m, bool deriv){
   Matrix res = initMatrix(m.shape[0], m.shape[1], false);
-  int i;
-  for(i = 0; i < res.shape[0]; i++){
+  
+  for(size_t i = 0; i < res.shape[0]; i++){
     res.data[i] = sigmoidVector(m.data[i], deriv);
   }
 
@@ -119,9 +116,8 @@ Matrix sigmoidMatrix(Matrix m, bool deriv){
 
 
 double meanMatrix(Matrix m){
-  int i;
   double mean = 0.0;
-  for(i = 0; i < m.shape[0]; i++){
+  for(size_t i = 0; i < m.shape[0]; i++){
     mean += meanVector(m.data[i]);
   }
   return mean/m.shape[0];
