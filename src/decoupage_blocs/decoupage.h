36e73907f82419ee04ec6e52b0f0bfcd7ef6fb78
Valentin <36827635+valentinrbn@users.noreply.github.com>
2018-11-13 23:54:51 +0100

    delete

--- src/decoupage_blocs/decoupage.h
+++
@@ -1,9 +0,0 @@
-#ifndef DECOUPAGE_H
-#define DECOUPAGE_H
-
-# include "../matrix/matrix.h"
-
-SDL_Surface* lines(SDL_Surface* img);
-SDL_Surface* columns(SDL_Surface *img, int b_line, int e_line);
-
-#endif
