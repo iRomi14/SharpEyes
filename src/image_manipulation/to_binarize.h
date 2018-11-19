30433db4070bac2d12dac145e6bde56b074b4128
Valentin <36827635+valentinrbn@users.noreply.github.com>
2018-11-19 21:45:39 +0100

    new otsu /!\test/!\

--- src/image_manipulation/to_binarize.h
+++ src/image_manipulation/to_binarize.h
@@ -3,4 +3,12 @@
 
 void to_binarize(SDL_Surface *image_surface);
 
+void greyscale(SDL_Surface *img);
+
+int getHisto(SDL_Surface *img, int* histo);
+
+int getThreshold(int *histo, int total);
+
+void otsu(SDL_Surface *img);
+
 #endif
