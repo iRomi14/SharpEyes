8b75ad828bb4ac1fdfee0f584032e0f72c7da742
Valentin <36827635+valentinrbn@users.noreply.github.com>
2018-11-29 18:25:54 +0100

    sdl_function.h added

--- src/image_manipulation/SDL_functions.h
+++ src/image_manipulation/SDL_functions.h
@@ -7,5 +7,7 @@
 void SDL_ExitError(const char *message);
 void SDL_ExitSupress(const char *message, SDL_Renderer *renderer, SDL_Window *fenetre);
 void PressedKey(void);
+SDL_Surface* copy_image(SDL_Surface *img);
+SDL_Surface* Resize(SDL_Surface *img);
 
 #endif
